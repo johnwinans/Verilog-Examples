387484f6e4cf87388a50dbb9e51342d0deaa4d7a
John Winans <john@winans.org>
2023-12-05 05:52:18 -0600

    Test the PLL on the 2057 board

---
+++ pll/counter.v
@@ -0,0 +1,21 @@
+module counter (
+    input wire clk,
+	input wire reset,
+    output wire [7:0] out
+	);
+
+    reg [31:0] counter = 32'b0;
+
+    //assign out = ~counter[28:21];
+    assign out = counter[27:20];
+    //assign out = counter[30:23];
+
+    always @ (posedge clk) 
+	begin
+		if (reset)
+        	counter <= 0;
+		else
+        	counter <= counter + 1;
+    end
+
+endmodule
