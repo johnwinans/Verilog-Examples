6250b4621d00bc8d2f683a7b68965f2e130cab10
John Winans <john@winans.org>
2023-12-31 10:10:12 -0600

    Half adder example

---
+++ halfadder/README.md
@@ -0,0 +1,4 @@
+Demonstrate adding a pullup resistor to the button input.
+
+Note that since the .pcf file is not used by test-benches.
+Therefore it can not interfere with our ability to compile and run it.
---
+++ halfadder/top.v
@@ -0,0 +1,37 @@
+//**************************************************************************
+//
+//    Copyright (C) 2023  John Winans
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//**************************************************************************
+
+`default_nettype none
+
+/**
+* This will calculate the suym of a and b.
+**************************************************************************/
+module top (
+    input wire      a,
+    input wire      b,
+    output wire     sum,
+    output wire     cy 
+    );
+
+    assign sum = a ^ b;
+    assign  cy = a & b;
+
+endmodule
